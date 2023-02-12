68d6eafb08dcc184de87e3adc0e9099b64cde1e6
馒头饭 <mhjlw@126.com>
2023-02-12 18:41:39 +0800

    :new:add mini-react

---
+++ prototype/mini-react/demo/which-react.js
@@ -0,0 +1,17 @@
+ import {
+  Component,
+  useReducer,
+  useState,
+  useEffect,
+  useLayoutEffect
+ } from './src/react'
+ import ReactDOM from './src/react-dom'
+
+ export {
+  ReactDOM,
+  Component,
+  useReducer,
+  useState,
+  useEffect,
+  useLayoutEffect
+ }

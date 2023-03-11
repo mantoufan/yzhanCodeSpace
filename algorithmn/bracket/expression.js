c54486406f0e609feb135557d0a6a8e706dff971
馒头饭 <mhjlw@126.com>
2023-03-11 22:29:59 +0800

    :new:closure 解析规则

--- algorithmn/bracket/expression.js
+++ algorithmn/bracket/expression.js
@@ -1,5 +1,6 @@
 let Reg = /([1-9][0-9]{0,}|0\.{0-9}{0,})|(\+)|(\-)|(\*)|(\/)|([(])|([)])/g
-module.exports = function parse(str) {
+
+exports.parse = function parse(str) {
   let r
   const list = []
   while (r = Reg.exec(str)) {
@@ -15,6 +16,7 @@ module.exports = function parse(str) {
   return stack
 }
 
+// 流式处理，处理一个就往外输出
 function expressionParse(list) {
   const stack = []
   const n = list.length
@@ -27,7 +29,7 @@ function expressionParse(list) {
       }
       let child
       while ((child = stack.pop()).type !== '(') {
-        symbol.children.unshift(child) // 规约 reduce （合并）
+        symbol.children.unshift(child) // 归约 reduce （合并）
       }
       stack.push(symbol) // 移入 shift （放回去）
     } else {
@@ -38,3 +40,75 @@ function expressionParse(list) {
 }
 // LL 移入、规约：从左到右
 // LR 移入从左到右，规约：从右到左 LR(0) 有终止标识
+
+/* Expression 的 closure
+   <Expression> := <Additive>
+     <Additive> := <Multiplicative> |
+                   <Multiplicative> "+" <Additive> |
+                   <Multiplicative> "-" <Additive>
+<Multiplicative> := <Primary> |
+                   <Primary> "*" <Multiplicative> |
+                   <Primary> "/" <Multiplicative>
+      <Primary> := <Number> |
+                   "(" <Expression> ")"
+terminal symbol := 终结符 <Number>
+non-terminial symbol := 非终结符 "("
+
+对于数学表达式，开头的只能是 Number 或 "("  
+对于 JavaScript 表达式，开头可以是 ECMA 206 中 附录 A.2
+
+
+1. 设计数据结构，存储规则
+
+*/
+const map = new Map([
+  ['Expression', [['Additive']]],
+  ['Additive', [['Additive'], ['Multiplicative'], ['Multiplicative', '+', 'Additive'], ['Multiplicative', '-', 'Additive']]],
+  ['Multiplicative', [['Primary'], ['Primary', '*', 'Multiplicative'], ['Primary', '/', 'Multiplicative']]],
+  ['Primary', [['Number'], ['(', 'Expression', ')']]]
+])
+
+exports.closure = symbol => {
+  const rules = []
+  const pool = [symbol]
+  const visited = new Set()
+  while (pool.length !== 0) {
+    const current = pool.shift()
+    if (map.has(current) === false) continue
+    if (visited.has(current) === true) continue
+    visited.add(current)
+    map.get(current).forEach(newRules => {
+      if (visited.has(newRules[0]) === false) rules.push(newRules)
+      pool.push(newRules[0])
+    })
+  }
+  return rules
+}
+
+const generateState = symbols => {
+  const states = []
+  symbols.forEach((nextSymbol, index) => {
+    states.push(realSymbol => {
+      if (realSymbol.type === nextSymbol) {
+        return states[index + 1]
+      } 
+    })
+  })
+  return states[0]
+}
+
+const start = symbol => {
+  if (symbol.type === 'Expression') {
+
+    return 
+  }
+}
+
+const eof = symbol => {
+  if (symbol.type === 'EOF') {
+
+  }
+}
+
+const success = () => success
+
--- algorithmn/bracket/test/expression.test.js
+++ algorithmn/bracket/test/expression.test.js
@@ -1,7 +1,14 @@
-const parse = require('../expression')
+const { parse, closure } = require('../expression')
 
 describe('Test LR Parser', () => {
   test('(1+(2*3))', () => {
     expect(parse('(1+(2*3))')[0]).toEqual({"children": [{"type": "number", "value": "1"}, {"type": "+", "value": ""}, {"children": [{"type": "number", "value": "2"}, {"type": "*", "value": ""}, {"type": "number", "value": "3"}], "type": ""}], "type": ""})
+  })
+})
+
+describe('Test closure', () => {
+  test('Expression', () => {
+    const result = closure('Expression')
+    expect(JSON.stringify(result)).toBe(JSON.stringify([["Additive"],["Multiplicative"],["Multiplicative","+","Additive"],["Multiplicative","-","Additive"],["Primary"],["Primary","*","Multiplicative"],["Primary","/","Multiplicative"],["Number"],["(","Expression",")"]]))
   })
 })
